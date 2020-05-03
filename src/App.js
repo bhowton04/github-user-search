fa9c9f849ead39906facbe515501a79ed7bc204c
Brad Howton <bhowton@insequenceinc.com>
2020-05-03 16:02:51 -0500

    got rid of component directories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import SearchResults from './components/SearchResults';
+import Header from './components/header.js';
+import SearchBar from './components/searchBar.js';
+import SearchResults from './components/searchResults.js';
 
 function App() {
   return (
